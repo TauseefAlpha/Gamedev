23e0a827b2be808ddd5baca806300af3b2a7f41e
TauseefAlpha <tauseefalpha@gmail.com>
2023-07-24 21:18:29 +0500

    secondcommit

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const nav = document.querySelector("nav")
+const mobilenav = document.querySelector("nav.mobilenavbar")
+const menuicon = document.querySelector(".menuicon")
+const closebtn = document.querySelector(".mobilemenu .closebtn")
+const mobilemenu = document.querySelector(".mobilemenu")
 
 window.addEventListener("scroll", (event) => {
     console.log("value of pageYOffset", window.pageYOffset);
     if (window.pageYOffset > 60) {
 
         nav.classList.add("scrolled")
+        mobilenav.classList.add("scrolled")
     }
     else {
         nav.classList.remove("scrolled")
+        mobilenav.classList.remove("scrolled")
     }
 
+})
+
+menuicon.addEventListener('click', () => {
+    mobilemenu.classList.add("active")
+})
+
+closebtn.addEventListener('click', () => {
+    mobilemenu.classList.remove("active")
 })
